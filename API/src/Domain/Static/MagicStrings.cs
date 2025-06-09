c9c97cb29818f1d4e7b1d58fad4a9bb4ba644754
ahmdaboelhassan <ahmedaboelhassan1000@gmail.com>
2025-06-09 20:15:26 +0300

    Fix CORS BUG

--- API/src/Domain/Static/MagicStrings.cs
+++ API/src/Domain/Static/MagicStrings.cs
@@ -5,7 +5,7 @@
        public static string StageConnectionStrings = "Stage";
        public static string ProductionConnectionStrings = "Production";
        public static string DevelopmentConnectionStrings = "Development";
-       public static string[] ProductionOrigins = { "https://balancer.runasp.net/","http://balancer.runasp.net/" };
+       public static string[] ProductionOrigins = { "https://balancer.runasp.net","http://balancer.runasp.net" };
        public static string[] DevelopmentsOrigins = { "http://localhost:4200","https://localhost:4200" };
     }
 }
